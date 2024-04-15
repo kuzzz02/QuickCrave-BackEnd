b4bb04138455e17aa1f4730f41b766140757efbc
kuzzz02 <1343275629@qq.com>
2024-04-16 02:46:49 +0800

    first initialization

---
+++ src/main/java/com/quickcravebackend/QuickCraveBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.quickcravebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCraveBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickCraveBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quickcravebackend/QuickCraveBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickcravebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickCraveBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
