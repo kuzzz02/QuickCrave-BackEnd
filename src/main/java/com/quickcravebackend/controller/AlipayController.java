6a6c53dc42f22d6c162524ce315390c27792dbbe
kuzzz02 <1343275629@qq.com>
2024-05-20 17:52:29 +0800

    change imageController to streaming output form

---
+++ src/main/java/com/quickcravebackend/controller/AlipayController.java
@@ -0,0 +1,9 @@
+package com.quickcravebackend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/alipay")
+public class AlipayController {
+}
